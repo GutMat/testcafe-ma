45e4db0a079bbb57e0f67dc0a0b4ff47b4d0d61b
GutMat <s199693@student.uek.krakow.pl>
2021-09-01 17:26:52 +0200

    test(rwa): add test of user login

--- test/rwa/index.js
+++ test/rwa/index.js
@@ -1,18 +1,22 @@
 import { Selector } from "testcafe";
 
-// Gdy Jestem na stronie startowej aplikacji
 fixture`RWA`.page`http://localhost:3000/`;
 
 test("Wykonanie transakcji na nowym koncie użytkownika", async (t) => {
+  // Gdy Jestem na stronie startowej aplikacji
+  await t.expect(Selector("h1").innerText).eql("Sign in");
+
   // Kiedy Wybiorę opcję założenia nowego konta
   await t.click(
     Selector("a")
       .withAttribute("data-test", "signup")
       .withText("Don't have an account? Sign Up")
   );
+  // Wtedy Powinienem zobaczyć formularz rejestracji nowego użytkownika  await t
+  await t
+    .expect(Selector("h1").withAttribute("data-test", "signup-title").innerText)
+    .eql("Sign Up");
 
-  // Wtedy Powinienem zobaczyć formularz rejestracji nowego użytkownika
-  Selector("h1").withAttribute("data-test", "signup-title").withText("Sign Up");
   await t
     // Kiedy Wprowadzę moje imię
     .typeText("#firstName", "Mateusz")
@@ -33,5 +37,23 @@ test("Wykonanie transakcji na nowym koncie użytkownika", async (t) => {
     .click(Selector("button"));
 
   // Wtedy Powinienem zobaczyć ekran startowy
-  Selector("h1").withAttribute("data-test", "signup-title").withText("Sign in");
+  await t.expect(Selector("h1").innerText).eql("Sign in");
+
+  await t
+    // Kiedy Wprowadzę moją nazwę użytkownika
+    .typeText("input#username", "GutMat")
+    // Oraz Wprowadze moje hasło
+    .typeText("input#password", "Test123$%");
+
+  // Wtedy Powinienem móc zalogować się do aplikacji
+  await t
+    .expect(Selector("button").hasAttribute("disabled"))
+    .notOk()
+    // Kiedy Zaloguje się do aplikacji
+    .click(Selector("button"));
+
+  //   cy.intercept("GET", "/transactions/public").as("transaction");
+  //   cy.get("@signinButton").click();
+  //   // Wtedy Powinienem zobaczyć ekran powitalny
+  //   cy.wait("@transaction");
 });
